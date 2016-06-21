73253e7251051fe2a274a66a68c2b1c111fedc74
surikat <jo@surikat.pro>
2016-06-21 11:49:41 +0200

    update version

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 	$js - asynchronous module definition framework
 			or just simple lightweight javascript dependencies manager
 	
-	@version 5.8
+	@version 5.9
 	@link http://github.com/redcatphp/js/
 	@author Jo Surikat <jo@surikat.pro>
 	@website http://redcatphp.com
